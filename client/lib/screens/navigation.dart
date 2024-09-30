2e558a3d04929580da0925814175b40c8d395ebc
mahinchaoffice <45087212+dilshankarunarathne@users.noreply.github.com>
2024-09-30 12:07:15 +0530

    Update navigation.dart

--- client/lib/screens/navigation.dart
+++ client/lib/screens/navigation.dart
@@ -8,7 +8,7 @@ class Navigation extends StatefulWidget {
 
 class _NavigationState extends State<Navigation> {
   static final CameraPosition _initialCameraPosition = CameraPosition(
-    target: LatLng(37.7749, -122.4194), // Example coordinates (San Francisco)
+    target: LatLng(37.7749, -122.4194),
     zoom: 14.4746,
   );
 
