2cec0f5f9094fd2e73d5f9bed4f948cdda50ab56
dilshankarunarathne <maleeshasparrow@gmail.com>
2024-10-26 14:39:21 +0530

    voice navigation done

--- client/lib/screens/navigation.dart
+++ client/lib/screens/navigation.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_mapbox_navigation/flutter_mapbox_navigation.dart';
 import 'package:location/location.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
@@ -24,6 +26,7 @@ class _NavigationScreenState extends State<NavigationScreen> {
   late MapBoxOptions _navigationOption;
   final SpeechToText _speechToText = SpeechToText();
   bool _isListening = false;
+  List<Map<String, dynamic>> _nearbyPlaces = [];
 
   Future<void> initialize() async {
     if (!mounted) return;
@@ -78,11 +81,73 @@ class _NavigationScreenState extends State<NavigationScreen> {
 
   void _onSpeechResult(SpeechRecognitionResult result) {
     final recognizedWords = result.recognizedWords.toLowerCase();
-    print('Recognized words: $recognizedWords');
+    print('--------------------Recognized words: $recognizedWords');
     if (recognizedWords.contains('go back')) {
       Navigator.pop(context);
     } else if (recognizedWords.contains('camera')) {
       Navigator.pushNamed(context, '/roam_mode');
+    } else if (recognizedWords.startsWith('navigate to ')) {
+      final placeName = recognizedWords.replaceFirst('navigate to ', '').trim();
+      print("**********************$placeName");
+      _navigateToPlace(placeName);
+    }
+  }
+
+  Future<void> _navigateToPlace(String placeName) async {
+    print("------------------ navigating to place: $placeName");
+    print('----------------Fetching nearby places...');
+    await _fetchNearbyPlaces();
+    print('------------------Nearby places fetched: $_nearbyPlaces');
+    final place = _nearbyPlaces.firstWhere(
+      (place) => place['name'].toLowerCase() == placeName.toLowerCase(),
+      orElse: () => {},
+    );
+
+    if (place.isNotEmpty) {
+      final double destinationLatitude = place['latitude'];
+      final double destinationLongitude = place['longitude'];
+      print(
+          "****----------------- voice command navigation successful $destinationLatitude");
+
+      await _startNavigation(destinationLatitude, destinationLongitude);
+    } else {
+      print('----------------- Place not found');
+    }
+  }
+
+  Future<void> _fetchNearbyPlaces() async {
+    try {
+      final String response = await rootBundle.loadString('assets/campus.json');
+      final data = json.decode(response);
+      final features = data['features'] as List<dynamic>;
+
+      print('---------------------------- got Features');
+
+      setState(() {
+        _nearbyPlaces = features
+            .map((feature) {
+              final name = feature['properties']['name'];
+              final latitude = feature['geometry']['coordinates'][1];
+              final longitude = feature['geometry']['coordinates'][0];
+
+              if (name != null && latitude != null && longitude != null) {
+                return {
+                  'name': name as String,
+                  'latitude': latitude as double,
+                  'longitude': longitude as double,
+                };
+              } else {
+                return null;
+              }
+            })
+            .where((place) => place != null)
+            .cast<Map<String, dynamic>>()
+            .toList();
+
+        print('Nearby places: $_nearbyPlaces');
+      });
+    } catch (e) {
+      print(e);
     }
   }
 
@@ -127,8 +192,8 @@ class _NavigationScreenState extends State<NavigationScreen> {
               children: [
                 // Button to start navigation (if needed)
                 ElevatedButton(
-                  onPressed: () =>
-                      _startNavigation(), // Implement navigation logic
+                  onPressed: () => _startNavigation(
+                      8.655370, 81.21151), // Example coordinates
                   style: ElevatedButton.styleFrom(
                     foregroundColor: Colors.white,
                     backgroundColor: const Color(0xFF004D40), // Text color
@@ -145,11 +210,8 @@ class _NavigationScreenState extends State<NavigationScreen> {
     );
   }
 
-  Future<void> _startNavigation() async {
-    // Define your destination coordinates
-    const double destinationLatitude = 8.655370; // Replace with your latitude
-    const double destinationLongitude = 81.21151; // Replace with your longitude
-
+  Future<void> _startNavigation(
+      double destinationLatitude, double destinationLongitude) async {
     // Get the current location
     Location location = Location();
 
