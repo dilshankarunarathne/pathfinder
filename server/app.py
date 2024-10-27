62e2b04d1ba866d766c9dae53797895e5486e136
dilshankarunarathne <maleeshasparrow@gmail.com>
2024-10-27 17:43:24 +0530

    distance estimation works

--- server/app.py
+++ server/app.py
@@ -4,6 +4,8 @@ from ultralytics import YOLO
 import cv2
 import numpy as np
 
+from static.distances import KNOWN_WIDTHS, FOCAL_LENGTH
+
 app = FastAPI()
 
 app.add_middleware(
@@ -27,6 +29,12 @@ classNames = ['person', 'bicycle', 'car', 'motorbike', 'aeroplane', 'bus', 'trai
               'teddy bear', 'hair drier', 'toothbrush']
 
 
+def calculate_distance(actual_width, object_width_in_image, focal_length):
+    # Estimate the distance
+    distance = (actual_width * focal_length) / object_width_in_image
+    return distance
+
+
 @app.post("/predict")
 async def predict_image(file: UploadFile = File(...)):
     contents = await file.read()
@@ -38,12 +46,42 @@ async def predict_image(file: UploadFile = File(...)):
 
     try:
         results = model(frame)
+
         object_names = []
+
+        pred = model(frame, augment=False)[0]
+
+        # Loop through the results
+        for r in results:
+            boxes = r.boxes  # Identifying boxes
+
+            for box in boxes:
+                # Bounding box
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # Convert to int values
+
+                # Width of object in the image (in pixels)
+                object_width_in_image = x2 - x1
+
+                cls = int(box.cls[0])  # Identifying class name
+
+                # Get the known width of the object class
+                known_width = KNOWN_WIDTHS.get(classNames[cls], None)
+
+                if known_width is not None:
+                    # Estimate the distance
+                    distance = calculate_distance(known_width, object_width_in_image, FOCAL_LENGTH)
+                    object_names.append(f"{classNames[cls]}: {distance} inches away")
+                else:
+                    object_names.append(classNames[cls])
+
         for r in results:
             boxes = r.boxes
             for box in boxes:
                 cls = int(box.cls[0])
                 object_names.append(classNames[cls])
+
+        print(object_names)
         return {"objects": object_names}
     except Exception as e:
         return {"error": str(e)}
